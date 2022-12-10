64003a8c58e3933bccc4bfce0855e0bf5d8e7268
bfyu <yubufan_1993@outlook.com>
2022-12-10 21:48:37 +0800

    feat:leetcode 题目刷题记录-9

---
+++ src/LeetCode_9.kt
@@ -0,0 +1,61 @@
+/**
+ * 给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
+
+回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+
+例如，121 是回文，而 123 不是。
+
+
+示例 1：
+
+输入：x = 121
+输出：true
+示例 2：
+
+输入：x = -121
+输出：false
+解释：从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+示例 3：
+
+输入：x = 10
+输出：false
+解释：从右向左读, 为 01 。因此它不是一个回文数。
+
+
+提示：
+
+-2^31 <= x <= 2^31 - 1
+ */
+class LeetCode_9 {
+    fun isPalindrome(x: Int): Boolean {
+        if (x < 0) {
+            return false
+        }
+        val deci = 10
+        var input = x
+        val str = arrayListOf<Char>()
+        while (input != 0) {
+            val mod = input % deci
+            str.add(mod.toChar())
+            input /= deci
+        }
+        var start = 0
+        var end = str.size - 1
+        for (index in 0 until str.size) {
+            if (str[start] == str[end]) {
+                start++
+                end--
+                continue
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+}
+
+fun main() {
+    val func = LeetCode_9()
+    val ret = func.isPalindrome(1000021)
+    print(ret)
+}
